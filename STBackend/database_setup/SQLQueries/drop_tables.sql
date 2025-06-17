26f92264c30ef27945a3f262bf390b89070b6f22
jessicaxu0605 <jessicaxu0605@gmail.com>
2025-06-17 16:51:50 -0400

    drop tables queries lol i forgor to save this

---
+++ STBackend/database_setup/SQLQueries/drop_tables.sql
@@ -0,0 +1,12 @@
+drop table if exists
+    users,
+    locations,
+    building_types,
+    amenities,
+    listings,
+    photos,
+    renter_profiles,
+    renter_on_listing,
+    listing_on_renter,
+    listing_amenities
+cascade;
